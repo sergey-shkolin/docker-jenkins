461a323c9f6d45e3766d39093e0cfe01c78fad20
Sergey Shkolin <sergey.shkolin@me.com>
2015-05-05 12:11:24 +0800

    add auth params

--- swarm/jenkins-slave-run.sh
+++ swarm/jenkins-slave-run.sh
@@ -7,9 +7,11 @@ JENKINS_SERVER=${JENKINS_SERVER:-$HOST_IP}
 JENKINS_PORT=${JENKINS_PORT:-8080}
 JENKINS_LABELS=${JENKINS_LABELS:-swarm}
 JENKINS_HOME=${JENKINS_HOME:-$HOME}
+JENKINS_USERNAME=${JENKINS_USERNAME:-user}
+JENKINS_PASSWORD=${JENKINS_PASSWORD:-password}
 
 # start the docker daemon
 /usr/local/bin/wrapdocker &
 
 # start swarm slave
-java -jar /home/jenkins/swarm-client-1.22-jar-with-dependencies.jar -fsroot "$JENKINS_HOME" -labels "$JENKINS_LABELS" -master http://$JENKINS_SERVER:$JENKINS_PORT $@
+java -jar /home/jenkins/swarm-client-1.22-jar-with-dependencies.jar -username $JENKINS_USERNAME -password $JENKINS_PASSWORD -fsroot "$JENKINS_HOME" -labels "$JENKINS_LABELS" -master http://$JENKINS_SERVER:$JENKINS_PORT $@
