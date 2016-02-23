552ed3ec4ec9b7c87fa6babf97b414a6bea0f6eb
George L. Yermulnik <yz@yz.kiev.ua>
2016-02-23 11:34:40 +0200

    update wrapdocker script and swarm-client

--- swarm/jenkins-slave-run.sh
+++ swarm/jenkins-slave-run.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-HOST_IP=$(ip route | grep ^default | awk '{print $3}')
+HOST_IP=$(ip route | awk '$1 == "default" {print $3}')
 JENKINS_SERVER=${JENKINS_SERVER:-$HOST_IP}
 JENKINS_PORT=${JENKINS_PORT:-8080}
 JENKINS_LABELS=${JENKINS_LABELS:-swarm}
@@ -14,4 +14,4 @@ JENKINS_PASSWORD=${JENKINS_PASSWORD:-password}
 /usr/local/bin/wrapdocker &
 
 # start swarm slave
-java -jar /home/jenkins/swarm-client-1.22-jar-with-dependencies.jar -username $JENKINS_USERNAME -password $JENKINS_PASSWORD -fsroot "$JENKINS_HOME" -labels "$JENKINS_LABELS" -master $JENKINS_SERVER:$JENKINS_PORT $@
+java -jar /home/jenkins/swarm-client-2.0-jar-with-dependencies.jar -username $JENKINS_USERNAME -password $JENKINS_PASSWORD -fsroot "$JENKINS_HOME" -labels "$JENKINS_LABELS" -master $JENKINS_SERVER:$JENKINS_PORT $@
